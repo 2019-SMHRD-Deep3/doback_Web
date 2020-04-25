61224f2998f9d690c11f7f3a1bca3818effea222
김준형 <freeday614@naver.com>
2020-04-25 17:58:43 +0900

    수정

--- Doback/src/controller/LoginService.java
+++ Doback/src/controller/LoginService.java
@@ -17,7 +17,7 @@ public class LoginService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("立加 己傍");
+//		System.out.println("立加 己傍");
 		response.setCharacterEncoding("EUC-KR");
 		PrintWriter out = response.getWriter();
 		
